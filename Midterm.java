c408b2c7ff76d35f16dcbc3db41c3c283586ea3a
Aaron <59898602+AaronCrum@users.noreply.github.com>
2021-03-08 14:49:21 -0800

    Add files via upload

---
+++ Midterm.java
@@ -0,0 +1,26 @@
+/* 
+ *  Name: Aaron Crum
+ *  CSU ID: 2698195 
+ *  CIS 265: Homework 1
+ *  Description: Write a Java Program for calculating the area of a polygon of given number of sides and length of side. 
+*/
+
+import java.util.Scanner;
+public class Polygon {
+  public static void main(String args[]) {
+     Scanner sc=new Scanner(System.in);
+     
+     //Enter number of sides
+     System.out.print("Enter the number of sides in polygon:");
+     double n=sc.nextDouble();
+     
+     //Enter length of sides 
+     System.out.print("Enter the length of sides in polygon:");
+     double ln=sc.nextDouble();
+    
+    double angle= Math.toRadians(180/n);
+    angle=Math.tan(angle);
+    double area=((ln*ln*n)/(4*angle));
+    System.out.print("Area of polygon is:"+area);
+  }
+}
